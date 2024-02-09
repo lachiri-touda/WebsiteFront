75b8a1b53305fe2881ecda1e2a5c1fbee55ed0ab
lachiri-touda <lachiritouda@gmail.com>
2024-02-09 17:17:38 +0100

    feat: add products

--- WebsiteFront/src/components/products/products.component.spec.ts
+++ WebsiteFront/src/components/products/products.component.spec.ts
@@ -1,21 +1,69 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
-import { ProductsComponent } from './products.component';
-
-describe('ProductsComponent', () => {
-  let component: ProductsComponent;
-  let fixture: ComponentFixture<ProductsComponent>;
-
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      declarations: [ProductsComponent]
-    });
-    fixture = TestBed.createComponent(ProductsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+/*<div class="products-section">
+  <div class="container text-left">
+    <h3>Nos Produits</h3>
+    <div class="row">
+      <div class="col-md-3 col-sm-12">
+        <div class="product-item">
+          <img src="./../../assets/test4.png" alt="Sachets" />
+          <p>Sachets - أكياس</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12">
+        <div class="product-item">
+          <img src="./../../assets/dentelle2.png" alt="Sachets en polypropylène" />
+          <p>Sachets en polypropylène - أكياس بولي بروبيلين</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12">
+        <div class="product-item">
+          <img src="./../../assets/dentelle2.png" alt="Dentelles" />
+          <p>Dentelles <span style="color: #e8bb78">- دانتيل</span></p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12">
+        <div class="product-item">
+          <img src="./../../assets/dentelle2.png" alt="Broderies" />
+          <p>Broderies - تطريز</p>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-3 col-sm-12">
+        <div class="product-item">
+          <img src="./../../assets/product_3.jpg" alt="Rubans" />
+          <p>Rubans - شرائط</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12">
+        <div class="product-item">
+          <img src="./../../assets/product_3.jpg" alt="Ciseaux" />
+          <p>Ciseaux - مقص</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12">
+        <div class="product-item">
+          <img src="./../../assets/product_3.jpg" alt="Agraffes" />
+          <p>Agraffes - دبابيس</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12">
+        <div class="product-item">
+          <img src="./../../assets/product_3.jpg" alt="Visline" />
+          <p>Visline - خيط الخياطة</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12">
+        <div class="product-item">
+          <img src="./../../assets/product_3.jpg" alt="Pinces" />
+          <p>Pinces - ملقط</p>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-12">
+        <div class="product-item">
+          <img src="./../../assets/product_3.jpg" alt="Guipures" />
+          <p>Guipures - دانتيل متموج</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>*/
